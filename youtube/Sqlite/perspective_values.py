7a9e052c5510cef94eb2abdb8a942ac8ec527421
EmanuelHappy <emanueljulianoms@gmail.com>
2019-08-02 19:58:34 -0300

    Good programing practices

--- youtube/Sqlite/perspective_values.py
+++ youtube/Sqlite/perspective_values.py
@@ -3,8 +3,6 @@ __email__ = "emanueljulianoms@gmail.com"
 
 import json
 import argparse
-import gc
-import copy
 
 from googleapiclient import discovery
 from requests import Session
@@ -16,13 +14,13 @@ from empath import Empath
 lexicon = Empath()
 
 parser = argparse.ArgumentParser(description="""This script creates a new sqlite database,
-                                                based on empath scores of each youtube comment.""")
+                                                based on perspective API scores of each youtube comment.""")
 
 parser.add_argument("--src", dest="src", type=str, default="/../../../scratch/manoelribeiro/helpers/text_dict.sqlite",
-                    help="Source folder of the comments.")
+                    help="Sqlite DataBase source of the comments.")
 
 parser.add_argument("--dst", dest="dst", type=str, default="perspective_value.sqlite",
-                    help="Where to save the output files.")
+                    help="Sqlite DataBase to store the perspective values.")
 
 parser.add_argument("--init", dest="init", type=int, default="0",
                     help="Comment where the analysis begin.")
@@ -30,14 +28,9 @@ parser.add_argument("--init", dest="init", type=int, default="0",
 parser.add_argument("--end", dest="end", type=int, default="-1",
                     help="Comment where the analysis end.")
 
-parser.add_argument("--commit", dest="commit", type=int, default="10000",
-                    help="Commit at some number of iterations.")
-
-parser.add_argument("--del", dest="del", type=int, default="5",
-                    help="Free memory at this number of iterations.")
-
 args = parser.parse_args()
 
+# Parameters for the perspective api
 
 attributes = ['TOXICITY', 'SEVERE_TOXICITY', 'IDENTITY_ATTACK', 'INSULT',
              'PROFANITY', 'THREAT', 'SEXUALLY_EXPLICIT',  'FLIRTATION']
@@ -49,41 +42,57 @@ data_dict = {'comment': {'text': ''},
              'languages': ['en'],
              'requestedAttributes': dict_attributes}
 
+
 def initialize_worker():
+    """
+    Initialize global session for the pools
+    :return:
+    """
     global session_global
     session_global = Session()
-    
-def process_json(text):
+
+
+def process_text(text):
+    """
+    Make a perspective request for the text in parallel
+    :param text: String that will be analysed by the perspective api
+    :return: Dictionary with the request values
+    """
     perspective_values = dict()
     data_dict['comment']['text'] = text
+
     session = session_global
     response = session.post(url=url, json=data_dict)
+
+    # The following part was added to slow the # of requests/second, so I can increase the # of multi-processing workers
     
     response_dict = json.loads(response.content)
-     
-    for attr in attributes:
-        perspective_values[attr] = response_dict['attributeScores'][attr]['summaryScore']['value']
+    
+    try:
+        for attr in attributes:
+            perspective_values[attr] = response_dict['attributeScores'][attr]['summaryScore']['value']
+    except:
+        print(text)
+        print(response)
+        print(response_dict)
         
     return perspective_values
 
-def multi_attr(response):
-    perspective_values = dict()
-    response_dict = json.loads(response.content)
-    
-    for attr in attributes:
-        perspective_values[attr] = response_dict['attributeScores'][attr]['summaryScore']['value']
-        
-    return perspective_values
-        
     
 def add_perspective(db1, db2):
+    """
+    Store in the output Database the values of the perspective api for each comment in the input Database
+    :param db1: Input Database
+    :param db2: Output Database
+    :return: None
+    """
     c = 0
     jsons_to_load = []
     id_list = []
 
     for id_c, values in db1.items():
         
-        c+=1
+        c += 1
         if c < args.init:
             continue      
             
@@ -92,15 +101,17 @@ def add_perspective(db1, db2):
         jsons_to_load.append(values['text'])
         
         if c == args.end:
-            print('start requests')
+
+            print('Requests initiated')
             t_req_i = time()
-            perspective_list = p.map(process_json, jsons_to_load)
+
+            perspective_list = p.map(process_text, jsons_to_load)
+
             t_req_e = time()
-            print(f"Time to finish the request: {round((t_req_e-t_req_i) / 60, 2)}")
-            
-            
-            for id_c, perspective_value in zip(id_list, perspective_list):
-                db2[id_c] = perspective_value
+            print(f"Time to finish the requests: {round((t_req_e-t_req_i) / 60, 2)}")
+
+            for id_c_out, perspective_value in zip(id_list, perspective_list):
+                db2[id_c_out] = perspective_value
 
             db2.commit()
             db2.close()
@@ -109,26 +120,30 @@ def add_perspective(db1, db2):
 
     
 if __name__ == '__main__':
-    print('start')
+
+    print('start')  # FeedBack
+
+    # Loading API KEY:
     with open('secrets.json') as file:
         api_key = json.load(file)['key']
-    
-    url = ('https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze' +
-       '?key=' + api_key)
-    
+
+    #  Connecting to the API:
+    url = ('https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze' + '?key=' + api_key)
     service = discovery.build('commentanalyzer', 'v1alpha1', developerKey=api_key)
-    
+
+    # Initiating the DataBases:
     dict_c = SqliteDict(args.src, tablename="text", flag="r")
     value_dict = SqliteDict(args.dst, tablename="value", journal_mode='OFF')
-    
-    p = Pool(20, initializer=initialize_worker)
-    p2 = Pool(16)
+
+    # Initiating multi-process pool:
+    workers = 20  # The number 20 was chosen because it best fit the # of requests/second
+    p = Pool(workers, initializer=initialize_worker)
     
     time_init = time()
-    
+
+    # Running Perspective
     add_perspective(dict_c, value_dict)
-    
+
+    # FeedBack at the end
     time_end = time()
     print(f"Time to finish the analysis: {round((time_end-time_init) / 60, 2)}")
-
-    
